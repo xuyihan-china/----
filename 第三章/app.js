91e4b417612db332cfcfebbc0aa374f03e43d14e
xuyihan-china <848247256@qq.com>
2020-10-23 21:04:29 +0800

    00

--- 第三章/app.js
+++ 第三章/app.js
@@ -1,7 +1,19 @@
 const http = require('http')
 http.createServer((req,res)=>{
+    //对 req/query 进行解析 
     const method = req.method
     const url = req.url
     const path = url.path.split('?')[0]
     const query = querystring.parse(url.path.split('?')[1])
+//设置回复类型 回复头 为JSON格式 application/json
+    res.setHeader('Content-type','application/json')
+    const resData ={
+        method,
+        url,
+        path,
+        query
+    }
+    if(method==='GET'){
+
+    }
 })
