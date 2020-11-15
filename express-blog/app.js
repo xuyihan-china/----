72ea96444e298afaabb0603340e84f639fc17ef4
xuyihan-China <848247256@qq.com>
2020-11-15 11:32:34 +0800

    unknown

--- express-blog/app.js
+++ express-blog/app.js
@@ -4,8 +4,8 @@ var path = require('path'); //path 路径文件
 var cookieParser = require('cookie-parser'); //解析cookie 省去操作
 var logger = require('morgan');//记录日志
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');//父路由 后面在users中添加
+//var indexRouter = require('./routes/index');
+//var usersRouter = require('./routes/users');//父路由 后面在users中添加
 var userRouter = require('./routes/user')
 var blogRouter =require('./routes/blog')
 
@@ -19,10 +19,10 @@ app.use(logger('dev'));
 app.use(express.json());//获取express 的body 的post请求 拿到postData 获取json格式
 app.use(express.urlencoded({ extended: false })); //post数据兼容其他的格式 （除了json）
 app.use(cookieParser());//解析cookie
-app.use(express.static(path.join(__dirname, 'public')));//拿到css等静态文件
+//app.use(express.static(path.join(__dirname, 'public')));//拿到css等静态文件
 //注册路由
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/', indexRouter);
+//app.use('/users', usersRouter);
 app.use('/api/user',userRouter)
 app.use('/api/blog',blogRouter)
 // catch 404 and forward to error handler 不在路由范围内报错
